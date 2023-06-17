8010f0b8e4ed945f6e88f96d37918a632e26665d
SmartShipping <smartshipping.app@gmail.com>
2023-06-17 13:03:38 +0200

    bug fix

--- pages/MyOrders/index.tsx
+++ pages/MyOrders/index.tsx
@@ -8,34 +8,34 @@ import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
 
 function Index() {
-  const router = useRouter();
-  const { encryptedOrderId } = router.query;
-  const orderId = decryptData(encryptedOrderId as string);
-  const { data: session }: { data: SessionExt | null } = useSession();
+  // const router = useRouter();
+  // const { encryptedOrderId } = router.query;
+  // const orderId = decryptData(encryptedOrderId as string);
+  // const { data: session }: { data: SessionExt | null } = useSession();
 
-  const { data } =useContractReads({
-    contracts: [
-      {
-        address: process.env.NEXT_PUBLIC_ORDER_MANAGER_ADDRESS as `0x${string}`,
-        abi: SmartShopperABI,
-        functionName: "getOrder",
-        args: [orderId],
-      },
-      {
-        address: process.env.NEXT_PUBLIC_ORDER_MANAGER_ADDRESS as `0x${string}`,
-        abi: SmartShopperABI,
-        functionName: "fees",
-      },
-    ],
-    onSuccess(data: [OrderSC, BigNumber]) {
-      if (data[0].orderId === "") {
-        // setCommissions(data[1].toNumber());
-        // showCheckout.current.hasFetchedSC = true;
-      } else {
-        router.push("/my-orders");
-      }
-    },
-  });
+  // const { data } =useContractReads({
+  //   contracts: [
+  //     {
+  //       address: process.env.NEXT_PUBLIC_ORDER_MANAGER_ADDRESS as `0x${string}`,
+  //       abi: SmartShopperABI,
+  //       functionName: "getOrder",
+  //       args: [orderId],
+  //     },
+  //     {
+  //       address: process.env.NEXT_PUBLIC_ORDER_MANAGER_ADDRESS as `0x${string}`,
+  //       abi: SmartShopperABI,
+  //       functionName: "fees",
+  //     },
+  //   ],
+  //   onSuccess(data: [OrderSC, BigNumber]) {
+  //     if (data[0].orderId === "") {
+  //       // setCommissions(data[1].toNumber());
+  //       // showCheckout.current.hasFetchedSC = true;
+  //     } else {
+  //       router.push("/my-orders");
+  //     }
+  //   },
+  // });
   return <div>index</div>;
 }
 
