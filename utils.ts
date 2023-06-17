193dc9b2711567d4dac7a19c5847dd89859c306f
SmartShipping <smartshipping.app@gmail.com>
2023-06-17 13:06:50 +0200

    bugfix

--- utils.ts
+++ utils.ts
@@ -1,13 +1,13 @@
-import { AES, enc } from "crypto-js";
+// import { AES, enc } from "crypto-js";
 
-export const decryptData = (data: string) => {
-  try {
-    const decodedStr = decodeURIComponent(data);
-    return AES.decrypt(
-      decodedStr,
-      process.env.NEXT_PUBLIC_API_ENCRYPTER!
-    ).toString(enc.Utf8);
-  } catch {
-    return "";
-  }
-};
+// export const decryptData = (data: string) => {
+//   try {
+//     const decodedStr = decodeURIComponent(data);
+//     return AES.decrypt(
+//       decodedStr,
+//       process.env.NEXT_PUBLIC_API_ENCRYPTER!
+//     ).toString(enc.Utf8);
+//   } catch {
+//     return "";
+//   }
+// };
